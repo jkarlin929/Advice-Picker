efc55597cb6c8d27f2148c6930bd299aac5d246f
jkarlin929 <osida11572@gmail.com>
2021-02-04 23:00:04 -0500

    add html to display advice but not displaying yet, created fetch resp and rendering value of advice in console

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// javascript goes here 
+// javascript goes here
+const advice = document.getElementById('advice');
+const URL = "https://api.adviceslip.com/advice";
+
+
+fetch(URL)
+.then((resp) => resp.json())
+  .then(function(data) {
+    console.log(data)
+    for (const [key, value] of Object.entries(data)) {
+      console.log(data.slip.advice);
+      const adviceSlip = `${data.slip.advice}`;
+    }
+
+
+  })
+.catch((er) => {
+
+});
