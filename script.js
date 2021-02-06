b6585596c88a8dce8296aa04acdefcb9d2e0f282
jkarlin929 <osida11572@gmail.com>
2021-02-06 12:12:02 -0500

    added results to display depending on button and coloring for yes and no on advice p tag

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 // javascript goes here
-const advice = document.getElementById('advice');
 const URL = "https://api.adviceslip.com/advice";
 
+function getResults() {
+  fetch(URL)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      console.log(data)
+      for (const [key, value] of Object.entries(data)) {
+        console.log(data.slip.advice);
+        const adviceSlip = `${data.slip.advice}`;
+        let adviceTag = document.querySelector('#advice');
+        adviceTag.style.color = "#cfc8b9";
+        adviceTag.innerHTML = adviceSlip
+      }
+    })
+    .catch(function(e) {
+      console.log(e);
+    });
+};
 
-fetch(URL)
-.then((resp) => resp.json())
-  .then(function(data) {
-    console.log(data)
-    for (const [key, value] of Object.entries(data)) {
-      console.log(data.slip.advice);
-      const adviceSlip = `${data.slip.advice}`;
-    }
+function yesResults() {
+  const yesButton = document.querySelector('#yes-button');
+  yesButton.addEventListener("click", function(e) {
+    getResults();
+  });
+};
+
+yesResults();
+
+function noResults() {
+  const noButton = document.querySelector('#no-button');
+  noButton.addEventListener("click", function(e) {
+    let adviceTag = document.querySelector('#advice');
+    adviceTag.style.color = "#FEF535";
+    adviceTag.innerHTML = "Are you sure?"
+  });
+};
+
+noResults();
 
 
-  })
-.catch((er) => {
-
-});
+//write a function
+//if yes button is clicked, display advice results
+//if no button is clicked, dispaly Are you Sure?
