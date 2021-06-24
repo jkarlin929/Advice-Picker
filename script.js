5a72573dc4c45d5813ff545ac29458290356ebe5
jkarlin929 <osida11572@gmail.com>
2021-06-23 23:31:41 -0400

    updated

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function getResults() {
   fetch(URL)
     .then((resp) => resp.json())
     .then(function(data) {
-      console.log(data)
+      // console.log(data)
       for (const [key, value] of Object.entries(data)) {
-        console.log(data.slip.advice);
+        // console.log(data.slip.advice);
         const adviceSlip = `${data.slip.advice}`;
         let adviceTag = document.querySelector('#advice');
         adviceTag.style.color = "#cfc8b9";
@@ -31,7 +31,9 @@ yesResults();
 function noResults() {
   const noButton = document.querySelector('#no-button');
   noButton.addEventListener("click", function(e) {
+    console.log(e.length)
     let adviceTag = document.querySelector('#advice');
+    let count = 0;
     adviceTag.style.color = "#FEF535";
     adviceTag.innerHTML = "Are you sure?"
   });
