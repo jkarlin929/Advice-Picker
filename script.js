e3abb157e639f9c7c18798fa7b0dd9be42652429
jkarlin929 <osida11572@gmail.com>
2021-07-20 22:15:56 -0400

    add reset button

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ function getResults() {
   fetch(URL)
     .then((resp) => resp.json())
     .then(function(data) {
-      // console.log(data)
       for (const [key, value] of Object.entries(data)) {
         const adviceSlip = `${data.slip.advice}`;
         let adviceTag = document.querySelector('#advice');
         adviceTag.style.color = "#cfc8b9";
         adviceTag.innerHTML = adviceSlip;
-
       }
     })
     .catch(function(e) {
@@ -19,29 +17,53 @@ function getResults() {
     });
 };
 
+function hideYesNo() {
+  const yesButton = document.querySelector('#yes-button');
+  const noButton = document.querySelector('#no-button');
+  const resetButton = document.querySelector('#reset-button');
+  yesButton.style.display = "none";
+  noButton.style.display = "none";
+  resetButton.style.display = "block";
+};
+
+function showYesNo() {
+  const yesButton = document.querySelector('#yes-button');
+  const noButton = document.querySelector('#no-button');
+  const resetButton = document.querySelector('#reset-button');
+  yesButton.style.display = "block";
+  noButton.style.display = "block";
+  resetButton.style.display = "none";
+};
+
+function reset() {
+  const resetButton = document.querySelector('#reset-button');
+  resetButton.addEventListener("click", function(e) {
+    showYesNo();
+  });
+};
+
 function yesResults() {
   const yesButton = document.querySelector('#yes-button');
   yesButton.addEventListener("click", function(e) {
     getResults();
+    hideYesNo();
+    reset();
   });
 };
 
 function noResults() {
   const noButton = document.querySelector('#no-button');
   noButton.addEventListener("click", function(e) {
-    console.log(e.length)
     let adviceTag = document.querySelector('#advice');
     let count = 0;
     adviceTag.style.color = "#FEF535";
-    let noAnswers = ["Then why are you here?", "Come on, click Yes, you won't regret it", "Are you sure about that?", "Take a minute to think that over and maybe you'll change your mind.", "I think you should reconsider, but what do I know?"];
+    let noAnswers = ["Then why are you here?", "Come on, you won't regret it", "You should start over.", "Are you sure about that?", "Take a minute to think that over and maybe you'll change your mind.", "I think you should reconsider, but what do I know?"];
     var item = noAnswers[Math.floor(Math.random() * noAnswers.length)];
     adviceTag.innerHTML = item
+    hideYesNo();
+    reset();
   });
 };
+
 yesResults();
 noResults();
-
-
-//write a function
-//if yes button is clicked, display advice results
-//if no button is clicked, dispaly Are you Sure?
